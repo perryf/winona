7519133f638b33d435d822f0d7272d0bf8a065fb
Sarah Yu <sarahyu0692@gmail.com>
2017-11-18 12:56:21 -0500

    add new and create for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,26 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
   def show
     @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to lists_path, notice: "#{@list.name} was successfuly created."
+    else
+      render :new
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
