78a0c59bea41e5ab734477983e4fe90cc5be3298
Sarah Yu <sarahyu0692@gmail.com>
2017-11-29 16:50:36 -0500

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :couldnt_find_record
+  rescue_from NoMethodError, with: :no_method_error
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   Tmdb::Api.key("206193c24923aa18bca03c3d3c630850")
+
+  private
+  def couldnt_find_record
+    redirect_to root_url, alert: "The record you are looking for does not exist."
+  end
+
+  def no_method_error
+    redirect_to root_url, alert: "Sorry, you can't do that..."
+  end
 end
